069393ad71f9ae9e597be2a7c473d817534ca7bc
Тилькерман <macbookair@MacBook-Air-macbook.local>
2026-09-24 13:17:33 +0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/great-idea/sw.js', { scope: '/great-idea/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
